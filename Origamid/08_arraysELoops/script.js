a3011e44bc183b01a52cb016102ee26f56d2eddd
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-07-23 01:07:33 -0300

    Aula 0208 Arrays e Loops 1

---
+++ Origamid/08_arraysELoops/script.js
@@ -0,0 +1,19 @@
+var videoGames = ["Switch", "PS5", "Xbox One", "PC"];
+
+videoGames.pop(); // Remove o ultimo item do array e retorna ele
+console.log(videoGames); // ["Switch", "PS5", "Xbox One"]
+
+videoGames.push("3DS"); // Adiciona um novo item no final do array
+console.log(videoGames); // ["Switch", "PS5", "Xbox One", "3DS"]
+
+// For Loop
+for (let x = 1; x <= 10; x++) {
+  console.log(x);
+}
+
+// While Loop
+var i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
